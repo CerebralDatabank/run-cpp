b6366e4e1fb14853cd04be6c2e5675d8806b824f
Gopal Othayoth <Gopal.Othayoth@outlook.com>
2022-10-11 12:54:43 -0500

    Add colon to match compile error output

--- run-cpp/run.cpp
+++ run-cpp/run.cpp
@@ -21,7 +21,7 @@ int main() {
   result = system("./bin/a.out");
   if (result != 0) {
     cout << endl << "\x1B[31m[ ERROR ]\x1B[0m Program run failed - check errors above.\x1B[0m" << endl;
-    cout << "          exit code \x1B[93m" << result << "\x1B[0m" << endl;
+    cout << "          exit code: \x1B[93m" << result << "\x1B[0m" << endl;
     return result;
   }
   cout << endl << "\x1B[32m[    OK ]\x1B[0m Program run succeeded!\x1B[0m" << endl;
